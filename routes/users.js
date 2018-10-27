620034978442886fe1fe73362383230047e4bd85
ronjonsilver <rpolus1@asu.edu>
2018-10-27 16:56:18 -0700

    Added nodemon package, and User Register

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport-jwt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 //Register
-router.get('/register',(req,res,next) => {
-    res.send('REGISTER');
+router.post('/register',(req,res,next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
+        status: req.body.status
+    });
+    User.addUser(newUser, (err, user) => {
+        if(err){
+            res.json({success: false, msg:"Failed to register user!"})
+        }
+        else if(user.status == 0){
+            res.json({success: true, msg:"Buyer Registered!"})
+        }
+        else if(user.status == 1){
+            res.json({success: true, msg:"Seller Registered!"})
+        }
+    });
 });
 
 //Authenticate
