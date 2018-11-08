b0a33874f1c74374caa4c7cbe89768f72dcf74a5
JanYohan <salihy005@gmail.com>
2018-11-07 23:14:28 -0700

    Angular for login, BuyerRegister and SellerRegister

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -5,21 +5,25 @@ import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponent } from './components/BuyerRegister/BuyerRegister.component';
 import { BuyerComponent } from './components/buyer/buyer.component';
 import { SellerComponent } from './components/seller/seller.component';
 import { HomeComponent } from './components/home/home.component';
 import { MakeRequestComponent } from './components/buyer/make-request/make-request.component';
 import { BuyerAccountComponent } from './components/buyer/buyer-account/buyer-account.component';
 import { SellerAccountComponent } from './components/seller/seller-account/seller-account.component';
+import { SellerRegisterComponent } from './components/SellerRegister/SellerRegister.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'register', component: RegisterComponent},
+  {path: 'BuyerRegister', component: RegisterComponent},
+  {path: 'SellerRegister', component: SellerRegisterComponent},
   {path: 'login', component: LoginComponent},
   {path: 'buyer', component: BuyerComponent},
   {path: 'seller', component: SellerComponent}
+
 ];
 
 @NgModule({
@@ -33,7 +37,9 @@ const appRoutes: Routes = [
     HomeComponent,
     MakeRequestComponent,
     BuyerAccountComponent,
-    SellerAccountComponent
+    SellerAccountComponent,
+    SellerRegisterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- angular-src/src/app/components/BuyerRegister/BuyerRegister.component.ts
+++ angular-src/src/app/components/BuyerRegister/BuyerRegister.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  templateUrl: './BuyerRegister.component.html',
+  styleUrls: ['./BuyerRegister.component.css']
 })
 export class RegisterComponent implements OnInit {
 
