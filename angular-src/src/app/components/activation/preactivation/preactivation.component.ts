ca3a83df0198eabc350da83b34e16bb0f51b48a2
ronjonsilver <rpolus1@asu.edu>
2018-11-17 16:14:24 -0700

    separate email sending file

---
+++ angular-src/src/app/components/activation/preactivation/preactivation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preactivation',
+  templateUrl: './preactivation.component.html',
+  styleUrls: ['./preactivation.component.css']
+})
+export class PreactivationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
