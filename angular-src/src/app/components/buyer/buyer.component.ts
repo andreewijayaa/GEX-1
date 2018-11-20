f6481924e7fa9cd2f7d1426f4740d552a696294b
ronjonsilver <ronjonsilver12@gmail.com>
2018-11-19 23:15:12 -0700

    Found a bug, but could not fix it yet

--- angular-src/src/app/components/buyer/buyer.component.ts
+++ angular-src/src/app/components/buyer/buyer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RegisterService } from '../../services/register.service';
-import { StoreFetchService } from '../../services/storeFetch.service'
+import { BuyerService } from '../../services/buyer.service';
 import { Router } from '@angular/router';
 
 
@@ -13,12 +13,12 @@ export class BuyerComponent implements OnInit {
   buyer: Object;
 
   constructor(private registerService: RegisterService,
-    private storeFetchService: StoreFetchService,
+    private buyerService: BuyerService,
     private router: Router) { }
 
   ngOnInit() {
-    console.log(this.storeFetchService.getBuyerProfile());
-    this.storeFetchService.getBuyerProfile().subscribe((profile: any) => {
+    console.log(this.buyerService.getBuyerProfile());
+    this.buyerService.getBuyerProfile().subscribe((profile: any) => {
       this.buyer = profile.data;
       console.log(profile.data.account_type);
     },
@@ -26,16 +26,18 @@ export class BuyerComponent implements OnInit {
         console.log(err);
         return false;
       });
+      // const currentTab = 'requests';
+
+      // requests(currentTab) {
+      //   this.currentTab = currentTab;
+      // }
+    
+      // history(currentTab) {
+      //   this.currentTab = currentTab;
+      // }
+
   }
 
-  currentTab = 'requests';
 
-  requests(currentTab) {
-    this.currentTab = currentTab;
-  }
-
-  history(currentTab) {
-    this.currentTab = currentTab;
-  }
 
 }
