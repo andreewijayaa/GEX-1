f0e056db434575e167ff395d20e986b5e15ad55f
Omar J. Lozoya <omarj.lozoya@Omars-MacBook-Pro.local>
2018-11-26 22:17:51 -0700

    Logout button active and merging files

--- angular-src/src/app/components/navbar/buyer-navbar/buyer-navbar.component.ts
+++ angular-src/src/app/components/navbar/buyer-navbar/buyer-navbar.component.ts
@@ -26,7 +26,7 @@ export class BuyerNavbarComponent implements OnInit {
       cssClass: 'alert-success',
       timeout: 3000
     });
-    this.router.navigate(['']);
+    this.router.navigate(['/login']);
     return false;
   }
 }
