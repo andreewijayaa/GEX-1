561ef89108d07594512d88692e9912f913c93679
ronjonsilver <ronjonsilver12@gmail.com>
2018-11-29 14:03:36 -0700

    Added Dropdown for seller

--- angular-src/src/app/components/seller/seller-services/seller-services/seller-services.component.ts
+++ angular-src/src/app/components/seller/seller-services/seller-services/seller-services.component.ts
@@ -5,23 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './seller-services.component.html',
   styleUrls: ['./seller-services.component.css']
 })
-export class SellerServicesComponent implements OnInit {	
-	codes: any;
+export class SellerServicesComponent implements OnInit {
+	codes = [
+    { code: 95141601, name: 'House' },
+    { code: 95141602, name: 'Mobile Home' },
+    { code: 91111603, name: 'Cooking/Food Prep'},
+    { code: 82141505, name: 'Computer Generated Design'},
+    { code: 82121503, name: 'Digital Printing'},
+    { code: 82101503, name: 'Magazine Advertising'},
+    { code: 50443200, name: 'Frozen Corn'},
+    { code: 42131700, name: 'Surgical Textiles'},
+    { code: 30111700, name: 'Plasters'},
+    { code: 23241600, name: 'Metal Cutting Tools'},
+	];
+
+	code: Number;
+
+	public onChange(event): void {  // event will give you full breif of action
+    const newVal = event.target.value;
+		this.code = newVal;
+		console.log(this.code);
+	}
+	
   constructor() { }
 
   ngOnInit() {
-		const codes = [
-			{ code: 95141601, name: 'House' },
-			{ code: 95141602, name: 'Mobile Home' },
-			{ code: 91111603, name: 'Cooking/Food Prep'},
-			{ code: 82141505, name: 'Computer Generated Design'},
-			{ code: 82121503, name: 'Digital Printing'},
-			{ code: 82101503, name: 'Magazine Advertising'},
-			{ code: 50443200, name: 'Frozen Corn'},
-			{ code: 42131700, name: 'Surgical Textiles'},
-			{ code: 30111700, name: 'Plasters'},
-			{ code: 23241600, name: 'Metal Cutting Tools'},
-		]; 
   }
 
 }
