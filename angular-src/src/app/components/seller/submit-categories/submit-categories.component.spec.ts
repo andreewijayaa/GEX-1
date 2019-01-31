00218ec6c0abec1f1a1d807162ab3eb1ecf336f8
Andre Wijaya <a.wi7aya@gmail.com>
2019-01-31 15:14:24 -0700

    Part 9 Front-end from Google Sheet

---
+++ angular-src/src/app/components/seller/submit-categories/submit-categories.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SubmitCategoriesComponent } from './submit-categories.component';
+
+describe('SubmitCategoriesComponent', () => {
+  let component: SubmitCategoriesComponent;
+  let fixture: ComponentFixture<SubmitCategoriesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SubmitCategoriesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SubmitCategoriesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
