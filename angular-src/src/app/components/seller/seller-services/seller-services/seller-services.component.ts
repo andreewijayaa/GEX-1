3aa13673e86eb5350478105f03fc2222cf810021
ronjonsilver <rpolus1@asu.edu>
2019-02-16 03:27:35 -0700

    Connected Seller Goods/Services to backend

--- angular-src/src/app/components/seller/seller-services/seller-services/seller-services.component.ts
+++ angular-src/src/app/components/seller/seller-services/seller-services/seller-services.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 import { SellerService } from '../../../../services/seller.service';
 import { NotifierService } from 'angular-notifier';
 import { Local } from 'protractor/built/driverProviders';
-
+const DefaultImageIcon = "https://raw.githubusercontent.com/ronjonsilver/MEANstackPractice/master/images.png";
 @Component({
   selector: 'app-seller-services',
   templateUrl: './seller-services.component.html',
@@ -16,20 +16,20 @@ export class SellerServicesComponent implements OnInit {
 
   // Temp codes for MVP - Kurgan
 	codes = [
-    { code: 78965422, name: 'Jewelry', image: 'https://picsum.photos/200', checked: false },
-    { code: 78965423, name: 'Necklaces (Jewelry)', image: 'https://picsum.photos/200', checked: false },
-    { code: 78965424, name: 'Rings (Jewelry)', image: 'https://picsum.photos/200', checked: false },
-    { code: 78965425, name: 'Earrings (Jewelry)', image: 'https://picsum.photos/200', checked: false },
-    { code: 68977451, name: 'Dolls', image: 'https://picsum.photos/200', checked: false },
-    { code: 67887941, name: 'Sculptures', image: 'https://picsum.photos/200', checked: false },
-    { code: 62145331, name: 'Scarves', image: 'https://picsum.photos/200', checked: false },
-    { code: 54887921, name: 'Blankets', image: 'https://picsum.photos/200', checked: false },
-    { code: 52871151, name: 'Socks', image: 'https://picsum.photos/200', checked: false },
-    { code: 50360051, name: 'Pencils', image: 'https://picsum.photos/200', checked: false },
-    { code: 49605401, name: 'Painting', image: 'https://picsum.photos/200', checked: false },
-    { code: 49605402, name: 'Oil (Painting)', image: 'https://picsum.photos/200', checked: false },
-    { code: 49605403, name: 'Watercolor (Painting)', image: 'https://picsum.photos/200', checked: false },
-    { code: 49605404, name: 'Acrlyic (Painting)', image: 'https://picsum.photos/200', checked: false }
+    { code: 78965422, name: 'Jewelry', image: DefaultImageIcon, checked: false },
+    { code: 78965423, name: 'Necklaces (Jewelry)', image: DefaultImageIcon, checked: false },
+    { code: 78965424, name: 'Rings (Jewelry)', image: DefaultImageIcon, checked: false },
+    { code: 78965425, name: 'Earrings (Jewelry)', image: DefaultImageIcon, checked: false },
+    { code: 68977451, name: 'Dolls', image: DefaultImageIcon, checked: false },
+    { code: 67887941, name: 'Sculptures', image: DefaultImageIcon, checked: false },
+    { code: 62145331, name: 'Scarves', image: DefaultImageIcon, checked: false },
+    { code: 54887921, name: 'Blankets', image: DefaultImageIcon, checked: false },
+    { code: 52871151, name: 'Socks', image: DefaultImageIcon, checked: false },
+    { code: 50360051, name: 'Pencils', image: DefaultImageIcon, checked: false },
+    { code: 49605401, name: 'Painting', image: DefaultImageIcon, checked: false },
+    { code: 49605402, name: 'Oil (Painting)', image: DefaultImageIcon, checked: false },
+    { code: 49605403, name: 'Watercolor (Painting)', image: DefaultImageIcon, checked: false },
+    { code: 49605404, name: 'Acrlyic (Painting)', image: DefaultImageIcon, checked: false }
   ];
   submitLabels: String[];
   codeArray: Number[];
@@ -39,12 +39,6 @@ export class SellerServicesComponent implements OnInit {
   codeNames: any[];
   buttonText: String = 'Subscribe to any products on Requiren';
 
-  // Used for the dropdown
-/* 	public onChange(event): void {  // event will give you full breif of action
-    const newVal = event.target.value;
-		this.code = newVal;
-	} */
-
   constructor(private sellerService: SellerService,
     private notifierService: NotifierService,
     private router: Router) { this.notifier = notifierService; }
@@ -53,133 +47,16 @@ export class SellerServicesComponent implements OnInit {
   ngOnInit() {
     this.submitLabels = [];
     this.codeArray = [];
-    var LocalArray = new Array();
-    this.None = false;
     // Get seller codes
     this.sellerService.getCode().subscribe((data: any) => {
       if (data.success) {
         if (data.codeList.length === 0) { // Seller does not have any codes yet
-          this.None = true;
         } else {
           this.codeList = data.codeList;
-          this.None = false;
-          var i, j = 0;
-          // Retrieve all seller current codes
-          // FOR MVP ONLY, will find a better and suffiecent way to perform this
-          for (i = 0; i < this.codeList.length; i++) {
-
-            for (j = 0; j < this.codes.length; j++) {
-              if (this.codes[j].code === this.codeList[i]) {
-                LocalArray.push(this.codes[j].name);
-            }
-            }
-          }
-          this.codeNames = LocalArray;
         }
       }
     });
   }
-
-  // // Checking which checkboxes are checked and upload the code - By: Andre Wijaya
-  // AddCode() {
-  //   // Jewelry
-  //   var element = <HTMLInputElement> document.getElementById('78965422');
-  //   if (element.checked === true) {
-  //     this.code = Number(element.value);
-  //     this.uploadCode();
-  //   }
-
-  //   // Necklaces (Jewelry)
-  //   var element = <HTMLInputElement> document.getElementById('78965423');
-  //   if (element.checked === true) {
-  //     this.code = Number(element.value);
-  //     this.uploadCode();
-  //   }
-
-  //   // Rings (Jewelry)
-  //   var element = <HTMLInputElement> document.getElementById('789654224');
-  //   if (element.checked === true) {
-  //     this.code = Number(element.value);
-  //     this.uploadCode();
-  //   }
-
-  //   // Earrings (Jewelry)
-  //   var element = <HTMLInputElement> document.getElementById('78965425');
-  //   if (element.checked === true) {
-  //     this.code = Number(element.value);
-  //     this.uploadCode();
-  //   }
-
-  //   // Dolls
-  //   var element = <HTMLInputElement> document.getElementById('68977451');
-  //   if (element.checked === true) {
-  //     this.code = Number(element.value);
-  //     this.uploadCode();
-  //   }
-
-  //   // Sculptures
-  //   var element = <HTMLInputElement> document.getElementById('67887941');
-  //   if (element.checked === true) {
-  //     this.code = Number(element.value);
-  //     this.uploadCode();
-  //   }
-
-  //   // Scarves
-  //   var element = <HTMLInputElement> document.getElementById('62145331');
-  //   if (element.checked === true) {
-  //     this.code = Number(element.value);
-  //     this.uploadCode();
-  //   }
-
-  //   // Blankets
-  //   var element = <HTMLInputElement> document.getElementById('54887921');
-  //   if (element.checked === true) {
-  //     this.code = Number(element.value);
-  //     this.uploadCode();
-  //   }
-
-  //   // Socks
-  //   var element = <HTMLInputElement> document.getElementById('52871151');
-  //   if (element.checked === true) {
-  //     this.code = Number(element.value);
-  //     this.uploadCode();
-  //   }
-
-  //   // Pencils
-  //   var element = <HTMLInputElement> document.getElementById('50360051');
-  //   if (element.checked === true) {
-  //     this.code = Number(element.value);
-  //     this.uploadCode();
-  //   }
-
-  //   // Painting
-  //   var element = <HTMLInputElement> document.getElementById('49605401');
-  //   if (element.checked === true) {
-  //     this.code = Number(element.value);
-  //     this.uploadCode();
-  //   }
-
-  //   // Oil (Painting)
-  //   var element = <HTMLInputElement> document.getElementById('49605402');
-  //   if (element.checked === true) {
-  //     this.code = Number(element.value);
-  //     this.uploadCode();
-  //   }
-
-  //   // Watercolor (Painting)
-  //   var element = <HTMLInputElement> document.getElementById('49605403');
-  //   if (element.checked === true) {
-  //     this.code = Number(element.value);
-  //     this.uploadCode();
-  //   }
-
-  //   // Acrlyic (Painting)
-  //   var element = <HTMLInputElement> document.getElementById('49605404');
-  //   if (element.checked === true) {
-  //     this.code = Number(element.value);
-  //     this.uploadCode();
-  //   }
-  // }
 
   AddCode() {
     const code = {
@@ -188,8 +65,8 @@ export class SellerServicesComponent implements OnInit {
 
     this.sellerService.setNewCode(code).subscribe((data: any) => {
       if (data.success) {
-        this.notifier.notify('success', 'Your New Code was submitted!');
-        this.router.navigate(['/seller/seller-services']);
+        this.notifier.notify('success', data.msg);
+        this.router.navigate(['/seller']);
       } else {
         this.notifier.notify('error', data.msg);
         this.router.navigate(['/seller/seller-services']);
@@ -240,5 +117,6 @@ export class SellerServicesComponent implements OnInit {
         }
       }
     }
+   console.log(this.codeArray);
   }
 }
