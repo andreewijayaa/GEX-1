1e23d3de883d69525f761d12968eb5f459c9472d
Bryan Vu <vu9295@live.com>
2019-02-22 19:18:17 -0700

    Fixed module conflict

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -44,7 +44,6 @@ import { FlashMessagesModule } from 'angular2-flash-messages';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatFormFieldModule, MatInputModule, MatButtonModule, MatOptionModule, MatSelectModule, MatExpansionModule, MatBadgeModule, MatDialogModule, MatProgressSpinnerModule, MatDividerModule, MatListModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AcceptOfferDialogComponent } from './components/buyer/buyer.component';
 import { SubmitOfferDialogComponent, OfferSubmittedDialogComponent, OfferSubmittedFailedDialogComponent, StipeAccountCreatedSuccessDialogComponent, StipeAccountCreatedFailedDialogComponent } from './components/seller/seller.component';
 import { BuyerCartComponent } from './components/buyer/buyer-cart/buyer-cart.component';
 import { MomentModule } from 'ngx-moment';
