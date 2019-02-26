69321cd37cb6b3b84e82f8789c9edc0cf35071b4
Omar Lozoya <23390638+omarlozoya@users.noreply.github.com>
2019-02-26 02:41:15 -0700

    npm installed new dependencies and change some of the charges function buyer.js

--- angular-src/src/app/components/buyer/make-request/make-request.component.ts
+++ angular-src/src/app/components/buyer/make-request/make-request.component.ts
@@ -1,19 +1,20 @@
 // By Roni
 // Buyer Request Submission
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NotifierService } from 'angular-notifier';
-import { BuyerService } from '../../../services/buyer.service';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-const DefaultImageIcon = "https://raw.githubusercontent.com/ronjonsilver/MEANstackPractice/master/images.png";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { NotifierService } from "angular-notifier";
+import { BuyerService } from "../../../services/buyer.service";
+import * as ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+const DefaultImageIcon =
+  "https://raw.githubusercontent.com/ronjonsilver/MEANstackPractice/master/images.png";
 class ImageSnippet {
   constructor(public src: string, public file: File) {}
 }
 
 @Component({
-  selector: 'app-make-request',
-  templateUrl: './make-request.component.html',
-  styleUrls: ['./make-request.component.css']
+  selector: "app-make-request",
+  templateUrl: "./make-request.component.html",
+  styleUrls: ["./make-request.component.css"]
 })
 export class MakeRequestComponent implements OnInit {
   private readonly notifier: NotifierService;
@@ -25,46 +26,109 @@ export class MakeRequestComponent implements OnInit {
 
   // Temp codes for MVP - Kurgan
   codes = [
-    { code: 78965422, name: 'Jewelry', image: DefaultImageIcon, checked: false },
-    { code: 78965423, name: 'Necklaces (Jewelry)', image: DefaultImageIcon, checked: false },
-    { code: 78965424, name: 'Rings (Jewelry)', image: DefaultImageIcon, checked: false },
-    { code: 78965425, name: 'Earrings (Jewelry)', image: DefaultImageIcon, checked: false },
-    { code: 68977451, name: 'Dolls', image: DefaultImageIcon, checked: false },
-    { code: 67887941, name: 'Sculptures', image: DefaultImageIcon, checked: false },
-    { code: 62145331, name: 'Scarves', image: DefaultImageIcon, checked: false },
-    { code: 54887921, name: 'Blankets', image: DefaultImageIcon, checked: false },
-    { code: 52871151, name: 'Socks', image: DefaultImageIcon, checked: false },
-    { code: 50360051, name: 'Pencils', image: DefaultImageIcon, checked: false },
-    { code: 49605401, name: 'Painting', image: DefaultImageIcon, checked: false },
-    { code: 49605402, name: 'Oil (Painting)', image: DefaultImageIcon, checked: false },
-    { code: 49605403, name: 'Watercolor (Painting)', image: DefaultImageIcon, checked: false },
-    { code: 49605404, name: 'Acrlyic (Painting)', image: DefaultImageIcon, checked: false }
+    {
+      code: 78965422,
+      name: "Jewelry",
+      image: DefaultImageIcon,
+      checked: false
+    },
+    {
+      code: 78965423,
+      name: "Necklaces (Jewelry)",
+      image: DefaultImageIcon,
+      checked: false
+    },
+    {
+      code: 78965424,
+      name: "Rings (Jewelry)",
+      image: DefaultImageIcon,
+      checked: false
+    },
+    {
+      code: 78965425,
+      name: "Earrings (Jewelry)",
+      image: DefaultImageIcon,
+      checked: false
+    },
+    { code: 68977451, name: "Dolls", image: DefaultImageIcon, checked: false },
+    {
+      code: 67887941,
+      name: "Sculptures",
+      image: DefaultImageIcon,
+      checked: false
+    },
+    {
+      code: 62145331,
+      name: "Scarves",
+      image: DefaultImageIcon,
+      checked: false
+    },
+    {
+      code: 54887921,
+      name: "Blankets",
+      image: DefaultImageIcon,
+      checked: false
+    },
+    { code: 52871151, name: "Socks", image: DefaultImageIcon, checked: false },
+    {
+      code: 50360051,
+      name: "Pencils",
+      image: DefaultImageIcon,
+      checked: false
+    },
+    {
+      code: 49605401,
+      name: "Painting",
+      image: DefaultImageIcon,
+      checked: false
+    },
+    {
+      code: 49605402,
+      name: "Oil (Painting)",
+      image: DefaultImageIcon,
+      checked: false
+    },
+    {
+      code: 49605403,
+      name: "Watercolor (Painting)",
+      image: DefaultImageIcon,
+      checked: false
+    },
+    {
+      code: 49605404,
+      name: "Acrlyic (Painting)",
+      image: DefaultImageIcon,
+      checked: false
+    }
   ];
   title: String;
   submitLabels: String[];
   codeArray: Number[];
-  description: String = 'Enter description of desired product';
+  description: String = "Enter description of desired product";
   deadline: Date;
-  buttonText: String = 'Submit Your Request to Sellers on Requiren';
+  buttonText: String = "Submit Your Request to Sellers on Requiren";
 
-  public onChange(event): void {  // Simple deadline selection - Bryan Vu
-    if (event.target.value === '2days') {
+  public onChange(event): void {
+    // Simple deadline selection - Bryan Vu
+    if (event.target.value === "2days") {
       this.deadline = new Date();
       this.deadline.setDate(this.deadline.getDate() + 2);
     }
-    if (event.target.value === '5days') {
+    if (event.target.value === "5days") {
       this.deadline = new Date();
       this.deadline.setDate(this.deadline.getDate() + 5);
     }
-    if (event.target.value === '1week') {
+    if (event.target.value === "1week") {
       this.deadline = new Date();
       this.deadline.setDate(this.deadline.getDate() + 7);
     }
   }
 
-  constructor(private buyerService: BuyerService,
+  constructor(
+    private buyerService: BuyerService,
     private notifierService: NotifierService,
-    private router: Router) {
+    private router: Router
+  ) {
     this.notifier = notifierService;
   }
 
@@ -74,20 +138,18 @@ export class MakeRequestComponent implements OnInit {
   }
 
   labelButton() {
-    var btnTxt = 'Submit Your Request to ';
+    var btnTxt = "Submit Your Request to ";
     var slsize = this.submitLabels.length;
     //console.log(this.submitLabels[0]);
     for (var n = 0; n < slsize; n++) {
       if (slsize <= 3) {
         btnTxt = btnTxt + this.submitLabels[n];
-        if (n < (slsize - 1)) {
+        if (n < slsize - 1) {
           btnTxt = btnTxt + ", ";
-        }
-        else {
+        } else {
           btnTxt = btnTxt + " ";
         }
-      }
-      else {
+      } else {
         if (n < 2) {
           btnTxt = btnTxt + this.submitLabels[n] + ", ";
         }
@@ -103,8 +165,6 @@ export class MakeRequestComponent implements OnInit {
     }
     this.buttonText = btnTxt + "Sellers on Requiren";
   }
-
-
 
   onCheckboxChange(option, event) {
     if (event.target.checked) {
@@ -125,18 +185,18 @@ export class MakeRequestComponent implements OnInit {
   // Request Submission
   onRequestSubmit() {
     console.log(this.title);
-    if (typeof this.title === 'undefined') {
+    if (typeof this.title === "undefined") {
       window.scrollTo(1, 1);
-      return this.notifier.notify('error', 'Please input a title.');
+      return this.notifier.notify("error", "Please input a title.");
     }
     if (this.codeArray.length <= 0) {
-      return this.notifier.notify('error', 'Please select a catagory.');
+      return this.notifier.notify("error", "Please select a catagory.");
     }
-    if (this.description === 'Enter description of desired product') {
-      return this.notifier.notify('error', 'Please input a description.');
+    if (this.description === "Enter description of desired product") {
+      return this.notifier.notify("error", "Please input a description.");
     }
     if (!this.deadline) {
-      return this.notifier.notify('error', 'Please select a deadline.');
+      return this.notifier.notify("error", "Please select a deadline.");
     }
 
     window.scrollTo(1, 1);
@@ -151,30 +211,29 @@ export class MakeRequestComponent implements OnInit {
     // Register Request
     this.buyerService.postBuyerRequest(request).subscribe((data: any) => {
       if (data.success) {
-        this.notifier.notify('success', 'Your Request was submitted!');
-        this.router.navigate(['/buyer']);
+        this.notifier.notify("success", "Your Request was submitted!");
+        this.router.navigate(["/buyer"]);
       } else {
-        this.notifier.notify('error', data.msg);
+        this.notifier.notify("error", data.msg);
       }
     });
   }
 
   // By: John
   // function for updating profile image
-  processFile(imageInput: any){
+  processFile(imageInput: any) {
     debugger;
     const file: File = imageInput.files[0];
     const reader = new FileReader();
 
-    reader.addEventListener('load', (event: any) => {
+    reader.addEventListener("load", (event: any) => {
       this.selectedFile = new ImageSnippet(event.target.result, file);
       this.buyerService.addRequestImage(this.selectedFile.file).subscribe(
-        (res) => {
+        res => {
           this.Image_Urls.push(res["imageUrl"]);
         },
-        (err) => {
-
-        })
+        err => {}
+      );
     });
 
     reader.readAsDataURL(file);
