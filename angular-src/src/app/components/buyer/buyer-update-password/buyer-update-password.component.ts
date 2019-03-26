8870bd44c710678d23de5d8e8e390882e5d66da0
ronjonsilver <ronjonsilver12@gmail.com>
2019-03-26 15:45:17 -0700

    Front end password change for both buyers and sellers

--- angular-src/src/app/components/buyer/buyer-update-password/buyer-update-password.component.ts
+++ angular-src/src/app/components/buyer/buyer-update-password/buyer-update-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NotifierService } from 'angular-notifier';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import { BuyerService } from '../../../services/buyer.service';
+import { ValidateService } from '../../../services/validate.service';
 
 @Component({
   selector: 'app-buyer-update-password',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buyer-update-password.component.css']
 })
 export class BuyerUpdatePasswordComponent implements OnInit {
+  oldPassword: any;
+  password: any;
+  passwordConfirm: any;
 
-  constructor() { }
+  private readonly notifier: NotifierService;
+   constructor(private route: ActivatedRoute,
+    private buyerService: BuyerService,
+    private notifierService: NotifierService,
+    private router: Router,
+    private validateService: ValidateService) {
+      this.notifier = notifierService;
+    }
 
   ngOnInit() {
   }
 
+
+  updatePassword() {
+    if (this.oldPassword === undefined) { return this.notifier.notify('error', 'Please input a valid password.'); }
+    if (this.password === undefined) { return this.notifier.notify('error', 'Please input a new password.'); }
+    if (this.password !== this.passwordConfirm) { return this.notifier.notify('error', 'Passwords do not match.'); }
+    if (!this.validateService.validatePassword(this.password)) {
+      this.notifier.notify('error', 'Password does not meet requirements');
+      return false;
+    }
+
+    this.buyerService.updatePassword(this.oldPassword, this.password, this.passwordConfirm).subscribe((data: any) => {
+        if (data.success || !data) {
+          this.notifier.notify('success', data.msg);
+          this.router.navigate(['/buyer/buyer-account']);
+        } else {
+          this.notifier.notify('error', data.msg);
+        }
+      });
+  }
 }
