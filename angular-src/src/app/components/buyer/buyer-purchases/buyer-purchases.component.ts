567b82f9bde01d9350081e70d078933541ee3e7c
Omar Lozoya <23390638+omarlozoya@users.noreply.github.com>
2019-03-31 00:55:58 -0700

    Started creating the buyer purchases front end page

--- angular-src/src/app/components/buyer/buyer-purchases/buyer-purchases.component.ts
+++ angular-src/src/app/components/buyer/buyer-purchases/buyer-purchases.component.ts
@@ -1,4 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject, Input, ViewChild } from '@angular/core';
+import { BuyerService } from '../../../services/buyer.service';
+import { RequestService } from '../../../services/request.service';
+import { MatDialog, MatDialogConfig, MAT_DIALOG_DATA, MatDialogRef, MatRadioModule, MatRadioButton, MatTableDataSource, MatTab, MatSort, MatPaginator } from '@angular/material';
+import { NotifierService } from 'angular-notifier';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
+export interface RequestElement {
+  title: String;
+  status: String;
+  deadline: String;
+  description: String;
+  request_images: [String];
+}
+
+export interface OfferElement {
+  title: String;
+  description: String;
+  price: String;
+  shippingPrice: String;
+  offerStatus: String;
+  offerAccepted: String;
+  created_at: Date;
+  request_ID: String;
+  expected_completion: String;
+  list_of_sellers_submitted_offers: [String];
+  offer_images: [String];
+}
+
+export interface OrderElement {
+
+}
 
 @Component({
   selector: 'app-buyer-purchases',
